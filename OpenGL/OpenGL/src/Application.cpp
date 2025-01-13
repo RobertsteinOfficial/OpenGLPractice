0f4835580c670331e8e790fbd2a18be2bef10f04
Roberto Venezia <robertovenezia95@gmail.com>
2025-01-13 16:48:16 +0100

    Custom shader implementation

--- OpenGL/OpenGL/src/Application.cpp
+++ OpenGL/OpenGL/src/Application.cpp
@@ -3,6 +3,61 @@
 
 #include <iostream>
 
+static unsigned int CompileShader(unsigned int type, const std::string& source)
+{
+	unsigned int id = glCreateShader(type);
+	//c_str punta all'inizio della stringa
+	const char* src = source.c_str();
+
+	glShaderSource(id, 1, &src, nullptr);
+	glCompileShader(id);
+	
+	//Error handling
+	int result;
+	glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+
+	//Se la compilazione è andata male, mi recupero il messaggio di errore e lo stampo
+	if (result == GL_FALSE)
+	{
+		int length;
+		glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+
+		//Voglio allocare l'array di char sullo stack, solo che non posso semplicemente fare
+		//char message[length]; perchè length è una variabile, non una costante. 
+		//Soluzione uno sarebbe allocare sullo heap, ma non mi piace
+		//Soluzione due
+		char* message = (char*)alloca(length * sizeof(char));
+
+		glGetShaderInfoLog(id, length, &length, message);
+		std::cout << "Failed to compile " << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << "shader" << std::endl;
+		std::cout << message << std::endl;
+
+		glDeleteShader(id);
+		return 0;
+	}
+
+	return id;
+}
+
+static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader)
+{
+	unsigned int program = glCreateProgram();
+	unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+	unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+	//Faccio attachment dei due shader al programma, linko e valido il programma
+	glAttachShader(program, vs);
+	glAttachShader(program, fs);
+
+	glLinkProgram(program);
+	glValidateProgram(program);
+
+	glDeleteShader(vs);
+	glDeleteShader(fs);
+
+	return program;
+}
+
 int main(void)
 {
 	GLFWwindow* window;
@@ -36,6 +91,8 @@ int main(void)
 		 0.5f, -0.5f
 	};
 
+	unsigned int VAO; glGenVertexArrays(1, &VAO); glBindVertexArray(VAO);
+
 	//Definisco un vertex buffer
 	unsigned int buffer;
 	glGenBuffers(1, &buffer);
@@ -43,7 +100,7 @@ int main(void)
 	//Seleziono (bind) il buffer, in questo caso sarà un array
 	glBindBuffer(GL_ARRAY_BUFFER, buffer);
 
-	//Fillo il buffer
+	//Fillo il buffer. Posso anche allocare solo e fillare poi, in caso
 	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
 
 	//Abilito l'array di attributi. Posso farlo anche prima di definire gli attributi,
@@ -52,6 +109,35 @@ int main(void)
 
 	//Definisco gli attributi del buffer. In questo caso,2D vertex positions
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+
+
+	//Creo gli shader. Un po' uno sbatti farlo come stringa, poi lo passo su un file
+	std::string vertexShader = R"glsl(
+		#version 330 core
+		
+		layout(location = 0) in vec4 position;
+		
+		void main()
+		{
+		gl_Position = position;
+		};
+		)glsl";
+
+
+
+	std::string fragmentShader = R"glsl(
+		#version 330 core
+		
+		layout(location = 0) out vec4 color;
+		
+		void main()
+		{
+		color = vec4(1.0, 0.0, 0.0, 1.0);
+		};
+		)glsl";
+
+	unsigned int shader = CreateShader(vertexShader, fragmentShader);
+	glUseProgram(shader);
 
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
